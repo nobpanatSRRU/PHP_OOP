71f502b0ef0f95c1ff126b094d5426631eb27011
Guy NPNx <guyxdz@gmail.com>
2023-01-22 20:31:22 +0700

    upload

---
+++ 01AttrP/index.php
@@ -0,0 +1,40 @@
+<?php
+
+class Employee{
+   private $name;
+   private $department;
+   private $salary;
+
+   //getters
+}
+
+
+$empr = new Employee();
+$empr->name = "Jenny";
+$empr->department = "Accounting";
+$empr->salary= "20000";
+
+echo "ชื่อพนักงาน = ".$empr->name."</br>";
+echo "แผนก = ".$empr->department."</br>";
+echo "เงินเดือน = ".$empr->salary."</br>";
+
+$empr2 = new Employee();
+$empr2->name = "Kong";
+$empr2->department = "IT";
+$empr2->salary= "45000";
+
+echo "ชื่อพนักงาน = ".$empr2->name."</br>";
+echo "แผนก = ".$empr2->department."</br>";
+echo "เงินเดือน = ".$empr2->salary."</br>";
+
+$empr3 = new Employee();
+$empr3->name = "Jo";
+$empr3->department = "Sale";
+$empr3->salary= "30000";
+
+echo "ชื่อพนักงาน = ".$empr3->name."</br>";
+echo "แผนก = ".$empr3->department."</br>";
+echo "เงินเดือน = ".$empr3->salary."</br>";
+
+
+?>
---
+++ README.md
@@ -0,0 +1,4 @@
+# PHP_OOP Learning
+#### 64222200111 นายนพนัฐ ตันยอด คณะอุตสาหกรรม สาขาเทคโนโลยีคอมพิวเตอร์
+![This is an image]("/images/Screenshot 2566-01-22 at 20.25.11.png")
+![This is an image]("/images/Screenshot 2566-01-22 at 20.25.47.png")
